93a1cfd999f6d8efc2edf1190a04d155ddcebddd
JC Estibariz <jestibariz@bluecatnetworks.com>
2022-09-26 18:26:00 -0400

    Remove IE support

--- babel.config.js
+++ babel.config.js
@@ -3,19 +3,5 @@ module.exports = {
 		test: {
 			presets: ['@babel/env'],
 		},
-		es5: {
-			comments: false,
-			presets: [
-				['@babel/env', {loose: true, modules: false, useBuiltIns: 'usage', corejs: 3, targets: {ie: '11'}}],
-				['minify'],
-			],
-		},
-		es6: {
-			comments: false,
-			presets: [
-				['@babel/env', {loose: true, modules: false, useBuiltIns: false, targets: {chrome: '69', firefox: '61'}}],
-				['minify'],
-			],
-		},
 	},
 };
